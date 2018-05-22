e511f641d27844b86bea648981b36b28121b8395
fsgit <fsgit@git.com>
2018-05-22 17:59:09 +0800

    1
    1

--- coin/lesson1/blockchain.py
+++ coin/lesson1/blockchain.py
@@ -13,7 +13,9 @@
 # }
 import hashlib
 import json
-from time import time
+from time import time, sleep
+
+from flask import Flask
 
 
 class Blockchain:
@@ -56,10 +58,36 @@ class Blockchain:
         proof = 0
         while self.valid_proof(last_proof, proof) is False:
             proof += 1
+        print(proof)
         return proof
 
     def valid_proof(self, last_proof: int, proof: int) -> bool:
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-
+        # sleep(0.01)
+        print(proof, guess_hash)
         return guess_hash[0:4] == "0000"
+
+
+app = Flask(__name__)
+
+
+@app.route('/index', methods=['GET'])
+def index():
+    return "Hello BlockChain"
+
+
+@app.route('/transactions/new', methods=['POST'])
+def newTransactions():
+    return "we'll add a new transctions"
+
+@app.route('/mine',methods=['GET'])
+def mine():
+    return "we'll mine a new block"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+# if __name__ == "__main__":
+#     testPow = Blockchain()
+#     testPow.proof_of_work(100)
